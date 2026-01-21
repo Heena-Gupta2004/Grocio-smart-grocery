ab6aa7ad5e71b12dc1dbeaf1697f4be3d1fbe02e
Heena Gupta <heenakumari9551@gmail.com>
2026-01-22 00:30:16 +0530

    Update table.jsx

--- backend/utility/table.jsx
+++ backend/utility/table.jsx
@@ -1,1 +1,19 @@
+import React from 'react';
+import "./App.css";
+import ListComponent from "./Components/ListComponent";
+import listdatas from './utility/listdata';
+import TableComponent from "./Components/TableComponents";
+import tableDatas from "./utility/tabledata";
 
+
+
+
+const App = () => {
+    return (
+       <>
+        <TableComponent tabledatas={tableDatas} />
+       </> 
+    );
+};
+
+export default App;
